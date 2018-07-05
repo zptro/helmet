c5035c9cb4ab134d19b50f863650d664378e8436
West Jens <Jens.West@hsl.fi>
2018-07-05 11:06:51 +0300

    Added matrix calculations to hassmat.

--- sijoittelu/Scripts/hassmat.py
+++ sijoittelu/Scripts/hassmat.py
@@ -1,13 +1,25 @@
-def hass_run (emme_modeller, scen_id, demand_mat_id):
+def hass_run (emme_modeller, scen_id, demand_mat_id, time_mat_id, length_mat_id, cost_mat_id):
     """Perform car assignment for one scenario."""
     emmebank = emme_modeller.emmebank
     scenario = emmebank.scenario(scen_id)
+    # create_matrix(matrix_id=time_mat_id,
+                  # matrix_name="hatim",
+                  # matrix_description="ha-aikamatr s="+str(scen_id),
+                  # default_value=0)
+    # create_matrix(matrix_id=length_mat_id,
+                  # matrix_name="halen",
+                  # matrix_description="ha-pituusmatr s="+str(scen_id),
+                  # default_value=0)
+    # create_matrix(matrix_id=cost_mat_id,
+                  # matrix_name="ruma",
+                  # matrix_description="ruuhkamaksumatr s="+str(scen_id),
+                  # default_value=0)
     spec = {
         "type": "SOLA_TRAFFIC_ASSIGNMENT",
         "classes": [
             {
                 "mode": "c",
-                "demand": "mf1",
+                "demand": demand_mat_id,
                 "generalized_cost": {
                     "link_costs": "@rumsi",
                     "perception_factor": 0.2
@@ -16,7 +28,7 @@ def hass_run (emme_modeller, scen_id, demand_mat_id):
                     "link_volumes": None,
                     "turn_volumes": None,
                     "od_travel_times": {
-                        "shortest_paths": "mf380"
+                        "shortest_paths": time_mat_id
                     }
                 },
                 "path_analyses": [
@@ -39,7 +51,7 @@ def hass_run (emme_modeller, scen_id, demand_mat_id):
                         "results": {
                             "selected_link_volumes": None,
                             "selected_turn_volumes": None,
-                            "od_values": "mf381",
+                            "od_values": length_mat_id,
                         },
                     },
                     {
@@ -61,7 +73,7 @@ def hass_run (emme_modeller, scen_id, demand_mat_id):
                         "results": {
                             "selected_link_volumes": None,
                             "selected_turn_volumes": None,
-                            "od_values": "mf370",
+                            "od_values": cost_mat_id,
                         },
                     },
                 ]
@@ -78,7 +90,27 @@ def hass_run (emme_modeller, scen_id, demand_mat_id):
             "normalized_gap": 0.005
         }
     }
+    print "Traffic assignment started..."
     car_assignment = emme_modeller.tool(
         "inro.emme.traffic_assignment.sola_traffic_assignment"
     )
-    car_assignment(spec)
+    car_assignment(spec, scenario)
+    print "Traffic assignment performed for scenario " + str(scen_id)
+    matrix_spec = {
+        "type": "MATRIX_CALCULATION",
+        "expression": time_mat_id+"-0.2*"+length_mat_id+"-6*"+cost_mat_id,
+        "result": time_mat_id,
+        "constraint": {
+            "by_value": None,
+            "by_zone": None,
+        },
+        "aggregation": {
+            "origins": None,
+            "destinations": None,
+        },
+    }
+    matcalc = emme_modeller.tool(
+        "inro.emme.matrix_calculation.matrix_calculator"
+    )
+    matcalc(matrix_spec, scenario)
+	print "Generalized cost transformed to time"
---
+++ sijoittelu/Scripts/run_hassmat.py
@@ -0,0 +1,12 @@
+import inro.emme.desktop.app as _app
+import inro.modeller as _m
+import hassmat
+
+emme_desktop = _app.start_dedicated(
+    project="C:\Helmet\HELMET_KEHI_31\sijoittelu\sijoittelu.emp", 
+    visible=False, 
+    user_initials="HSL"
+)
+emme_modeller = _m.Modeller(emme_desktop)
+
+hassmat.hass_run(emme_modeller, 21, "mf1", "mf380", "mf381", "mf370")
